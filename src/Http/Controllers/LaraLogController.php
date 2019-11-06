fe0771116bb77d38767729dbff7f689a88ec1f17
Morteza Nasiri <n.mortezapc@gmail.com>
2019-11-07 00:07:39 +0330

    Dailylogs Revers!

--- src/Http/Controllers/LaraLogController.php
+++ src/Http/Controllers/LaraLogController.php
@@ -43,7 +43,7 @@ class LaraLogController extends Controller
             $paginatedSingleLogs->setPath($request->url());
         } else {
             $currentPage = LengthAwarePaginator::resolveCurrentPage();
-            $logsCollection = collect($this->dailyLogs[$logQuery]);
+            $logsCollection = collect($dailyLogs[$logQuery]);
             $perPage = 5;
             $currentPageItems = $logsCollection->slice(($currentPage * $perPage) - $perPage, $perPage)->all();
             $paginatedDailyLogs = new LengthAwarePaginator($currentPageItems , count($logsCollection), $perPage);
