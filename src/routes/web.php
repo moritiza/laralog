382056af5939f5e9b9954e9bb7d750c2c1d66803
Scrutinizer Auto-Fixer <auto-fixer@scrutinizer-ci.com>
2019-11-07 19:41:32 +0000

    Scrutinizer Auto-Fixes
    This commit consists of patches automatically generated for this project on https://scrutinizer-ci.com

--- src/routes/web.php
+++ src/routes/web.php
@@ -2,6 +2,6 @@
 
 // Auth::routes();
 
-Route::middleware(['web'])->namespace('MoriTiza\LaraLog\Http\Controllers')->group(function () {
+Route::middleware(['web'])->namespace('MoriTiza\LaraLog\Http\Controllers')->group(function() {
     Route::get('logs', 'LaraLogController@index');
 });
